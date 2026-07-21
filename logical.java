bfbd95b7d8b20c58adfc378cac0631c9e69f5ffd
varshiniak107-pixel <varshiniak107@gmail.com>
2026-07-21 15:55:48 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,14 @@
+public class logical{
+   public static void main(String[]args)
+{
+         int a,b,c,d;
+         a=10;
+         b=20;
+         c=15;
+         d=20;
+         System.out.println((a>b)&&(c<d));
+         System.out.println((a>b)||(c<d));
+         System.out.println(!(a==b));
+
+         }
+}
