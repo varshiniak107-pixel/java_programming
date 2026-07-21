241d5ee29d3d4ca205174c0bcde3db07cec923f5
varshiniak107-pixel <varshiniak107@gmail.com>
2026-07-21 15:46:53 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,15 @@
+public class arithmatic{
+   public static void main(String[]args)
+{
+         int a,b,c;
+         a=10;
+         b=20;
+         c=a+b;
+         System.out.println(c);
+         System.out.println(a-b);
+         System.out.println(a*b);
+         System.out.println(a/b);
+         System.out.println(a%b);
+
+         }
+}
