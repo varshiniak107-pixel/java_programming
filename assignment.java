27d20c4d66933d15fdea4daac192ebaacf384338
varshiniak107-pixel <varshiniak107@gmail.com>
2026-07-21 15:32:49 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,9 @@
+public class assignment{
+   public static void main(String[]args){
+         int a=10;
+         System.out.println(a);
+         System.out.println(a+10);
+         a-=b;
+         System.out.println(a-=b);
+         }
+}
