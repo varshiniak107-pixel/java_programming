b04410d6bb20ed2043ac590d13cda3019e84bce3
varshiniak107-pixel <varshiniak107@gmail.com>
2026-07-21 15:35:34 +0530

    Add files via upload

---
+++ unary.java
@@ -0,0 +1,9 @@
+public class unary{
+   public static void main(String[]args){
+         int a=10;
+         System.out.println(++a);
+         System.out.println(a);
+         System.out.println(a++);
+         System.out.println(a);
+         }
+}
