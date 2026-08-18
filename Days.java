d5907ee6e59bb1641b85d8aaf2ad39e810fceb20
varshiniak107-pixel <varshiniak107@gmail.com>
2026-08-18 14:31:09 +0530

    Add files via upload

---
+++ Days.java
@@ -0,0 +1,33 @@
+
+class Days {
+    public static void main(String[] args) {
+        System.out.print("Enter day number (1-7): ");
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day number");
+        }
+    }
+}
